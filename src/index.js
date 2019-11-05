9566ee4c9faa81392adf0364c4a1833df32c692f
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-11-05 10:19:38 +0100

    Create a single card component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 

2216870a4923601abbe5393eaf7bb35db36fd61c
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-11-05 12:48:29 +0100

    Change App component from function to class
    * add state to app class
    * create onSearchChange to dynamic update robot list using search field

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <Fragment>
-            <header>
-                <h1>RoboFriends</h1>
-                <SearchBox />
-            </header>
-            <main>
-                <CardList robots={robots} />
-            </main>
-        </Fragment>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return (
+            <Fragment>
+                <header className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                </header>
+                <main className='tc'>
+                    <CardList robots={filteredRobots} />
+                </main>
+            </Fragment >
+        );
+    }
+
 }
 
 export default App;

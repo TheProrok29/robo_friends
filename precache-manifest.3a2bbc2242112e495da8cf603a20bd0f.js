ddd840e29cd07fc17f6e0b1b4069e25985f08974
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-11-07 13:06:09 +0100

    Updates

---
+++ precache-manifest.3a2bbc2242112e495da8cf603a20bd0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97660ccf4023c2fee2258cb6c11dd248",
+    "url": "/robo_friends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robo_friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f90c0467f2e473e22a03",
+    "url": "/robo_friends/static/css/main.b5f5ca47.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robo_friends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "f90c0467f2e473e22a03",
+    "url": "/robo_friends/static/js/main.e69abac8.chunk.js"
+  },
+  {
+    "revision": "3972595ff8898eaa31c2",
+    "url": "/robo_friends/static/js/runtime-main.f087c8bb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo_friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
